e83273c71a4000388dcad83e60d2b04a3ad86b40
billaud <billaudwilliam@gmail.com>
2019-05-15 21:21:21 +0200

    Correction install kubectl

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var http=require("http");
 
 http.createServer(function (request, response) {
-     // Send the HTTP header 
+     // Send the HTTP header
   //    // HTTP Status: 200 : OK
   //       // Content Type: text/plain
        response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-     response.end('Hello World TER\n');
+     response.end('Hello World 4\n');
        }).listen(8081);
   //
   //                   // Console will print the message
